81fe08c531b7835908a30e9c8cbfb60516b5d0b9
Reni <Reni@RENIPC>
2021-12-20 12:57:32 +0300

    Добавьте файлы проекта.

---
+++ SoloApp/Character.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloApp
+{
+    //Класс
+    class Character
+    {
+        private string CharName { get; set; }
+        private EyeColor EyesColor { get; set; }
+        private PlotRole Role { get; set; }
+        private BattleSpecialty Spec { get; set; }
+        private MagicColor MagicColor { get; set; }
+        private Race Race { get; set; }
+        private int PowerLevel { get; set; }
+        //Конструктор
+        public Character(string name, EyeColor eyesColor, PlotRole role, BattleSpecialty battleSpec, MagicColor magicColor, Race race, Int16 powerN)
+        {
+            CharName = name;
+            EyesColor = eyesColor;
+            Role = role;
+            Spec = battleSpec;
+            MagicColor = magicColor;
+            Race = race;
+            PowerLevel = powerN;
+        }
+        //Принтануть всех персонажей
+        public void PrintChar()
+        {
+            Console.WriteLine($"{CharName}, {EyesColor}, {Role}, {Spec}, {MagicColor}, {Race}, {PowerLevel}");
+        }
+        //Принтануть только хуманов
+        public void PrintHumans()
+        {
+            if (Race == Race.Human)
+           Console.WriteLine($"{CharName}, {EyesColor}, {Role}, {Spec}, {MagicColor}, {Race}, {PowerLevel}");
+        }
+        //Принтануть только вампиров
+        public void PrintVamps()
+        {
+            if (Race == Race.Vampire)
+                Console.WriteLine($"{CharName}, {EyesColor}, {Role}, {Spec}, {MagicColor}, {Race}, {PowerLevel}");
+        }
+    }
+//Енумы
+enum EyeColor
+{
+    Black,
+    Brown,
+    Blue,
+    Yellow,
+    Red,
+    Blind,
+    Green,
+    Other
+}
+enum PlotRole
+{
+    Protagonist,
+    Deuteragonist,
+    SideCharacter
+}
+enum BattleSpecialty
+{
+    Fencer,
+    Knight,
+    Mage,
+    Caster,
+    Assassin,
+    Unique
+}
+enum MagicColor
+{
+        Classic,
+        MultiColored,
+        SlightlyColored,
+        SoulColored
+}
+enum Race
+    {
+        Human,
+        Elf,
+        Demon,
+        DemonElf,
+        DemonHuman,
+        HumanElf,
+        Vampire,
+        Other
+    }
+}
---
+++ SoloApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace SoloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Персонажи
+            Character rikkard = new Character("Rikkard Kronell", EyeColor.Brown, PlotRole.Protagonist, BattleSpecialty.Unique, MagicColor.SlightlyColored, Race.Human, 4);
+            Character veiland = new Character("Veiland Milten", EyeColor.Blue, PlotRole.Deuteragonist, BattleSpecialty.Knight, MagicColor.SoulColored, Race.Human, 8);
+            Character dalara = new Character("Dalara Ala'Irei", EyeColor.Other, PlotRole.Deuteragonist, BattleSpecialty.Unique, MagicColor.SlightlyColored, Race.DemonElf, 7);
+            Character kyle = new Character("Kyle", EyeColor.Blind, PlotRole.SideCharacter, BattleSpecialty.Mage, MagicColor.Classic, Race.DemonHuman, 7);
+            Character fredericka = new Character("Fredericka Astoni", EyeColor.Green, PlotRole.Deuteragonist, BattleSpecialty.Mage, MagicColor.SoulColored, Race.Human, 5);
+            Character jaden = new Character("Jaden", EyeColor.Blue, PlotRole.SideCharacter, BattleSpecialty.Caster, MagicColor.SoulColored, Race.Human, 9);
+            Character elsdeft = new Character("Elsdeft", EyeColor.Other, PlotRole.SideCharacter, BattleSpecialty.Caster, MagicColor.MultiColored, Race.Elf, 8);
+            Character lira = new Character("Lira", EyeColor.Blue, PlotRole.SideCharacter, BattleSpecialty.Unique, MagicColor.SoulColored, Race.Vampire, 10);
+            Character raven = new Character("Raven", EyeColor.Green, PlotRole.SideCharacter, BattleSpecialty.Assassin, MagicColor.Classic, Race.Human, 5);
+            Character kord = new Character("Kord Dakkari", EyeColor.Black, PlotRole.SideCharacter, BattleSpecialty.Fencer, MagicColor.Classic, Race.Human, 3);
+            Character dogma = new Character("Dogma", EyeColor.Other, PlotRole.SideCharacter, BattleSpecialty.Caster, MagicColor.SoulColored, Race.Elf, 10);
+            Character olrik = new Character("Olrik", EyeColor.Yellow, PlotRole.SideCharacter, BattleSpecialty.Mage, MagicColor.SoulColored, Race.Human, 9);
+            Character aphyr = new Character("Aphyr Allein", EyeColor.Red, PlotRole.SideCharacter, BattleSpecialty.Assassin, MagicColor.Classic, Race.Vampire, 9);
+            Character eri = new Character("Erika Starlight", EyeColor.Blue, PlotRole.SideCharacter, BattleSpecialty.Unique, MagicColor.SlightlyColored, Race.Human, 7);
+            Character affyn = new Character("Affyn II", EyeColor.Black, PlotRole.SideCharacter, BattleSpecialty.Knight, MagicColor.MultiColored, Race.Human, 7);
+            Character avelius = new Character("Avelius", EyeColor.Yellow, PlotRole.SideCharacter, BattleSpecialty.Unique, MagicColor.SoulColored, Race.Other, 11);
+            Character[] array = new Character[] { rikkard, veiland, dalara, kyle, fredericka, jaden, elsdeft, lira, raven, kord, dogma, olrik, aphyr, eri, affyn, avelius };
+            Console.WriteLine("\nMethod - Print All Characters:\n");
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i].PrintChar();
+            }
+            Console.WriteLine("\nMethod - Print All Humans:\n");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i].PrintHumans();
+            }
+            Console.WriteLine("\nMethod - Print All Vampires:\n");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i].PrintVamps();
+            }
+            Console.ReadKey();
+        }
+    }
+}
